7ebd04fcc4cde2d739be260b4d2d56118cd1db7b
Oleksandr <alexeev.army@gmail.com>
2023-04-02 16:10:01 +0300

    second commit main

--- MySuperApp.java
+++ MySuperApp.java
@@ -3,6 +3,7 @@ public class MySuperApp {
 	static int x;
 	static int y;
 	static int d;
+	static int e;
 	
 	
 	public static void main (String[] args) {
@@ -10,8 +11,10 @@ public class MySuperApp {
 		x = 1;
 		y = 3;
 		d = x + y;
+		e = d + x;
 		
 		System.out.println("D = " + d);	
+		System.out.println("E = " + e);
 	}
 }
 		
