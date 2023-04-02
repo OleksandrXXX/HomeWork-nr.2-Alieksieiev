f3c13df5773ca814b5dd41f0674c4606fe76b1b1
Oleksandr Alieksieiev <116300670+OleksandrXXX@users.noreply.github.com>
2023-04-02 16:04:06 +0300

    Add files via upload

---
+++ MySuperApp.java
@@ -0,0 +1,17 @@
+public class MySuperApp {
+	
+	static int x;
+	static int y;
+	static ind d;
+	
+	
+	public static void main (String[] args) {
+		
+		x = 1;
+		y = 3;
+		d = x + y;
+		
+		System.out.println(d);	
+	}
+}
+		
