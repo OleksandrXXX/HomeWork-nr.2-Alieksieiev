f52a861650162cf7d4b731f7cc40de68d7614f06
Oleksandr <alexeev.army@gmail.com>
2023-04-02 16:29:25 +0300

    third commit dev-2

--- MySuperApp.java
+++ MySuperApp.java
@@ -3,6 +3,7 @@ public class MySuperApp {
 	static int x;
 	static int y;
 	static int d;
+	static int w;
 	static String name;
 	static String name1;
 	static String name2;
@@ -15,6 +16,7 @@ public class MySuperApp {
 		x = 1 + 5;
 		y = 3 + 8;
 		d = x - y;
+		w = d + y + x;
 		name = "My";
 		name1 = "Super";
 		name2 = "App";
@@ -22,6 +24,7 @@ public class MySuperApp {
 
 		
 		System.out.println("D = " + d);
+		System.out.println("W = " + w);
         System out.println(name + " " + name1 + " " + name2);
         System.out.println(line);		
 	}
